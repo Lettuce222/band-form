add8cc3b6ba6b70bc246d55c4628d82499ee050a
宮地航太 <kmiyachi@miyachi.local>
2019-09-04 13:28:26 +0900

    get any json from frontend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <any msg="Welcome to Your Vue.js App" />
-    <button class="btn btn-primary">test</button>
+    <bandform></bandform>
   </div>
 </template>
 
 <script>
-import any from "./components/HelloWorld.vue";
+import bandform from "./components/BandForm.vue";
 export default {
   name: "app",
   components: {
-    any
+    bandform
   }
 };
 </script>
